7b06e96e1a8573b294ae1fff43139c2e6b7fce42
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-09 12:50:25 -0300

    fix: removed the notfound routes, and changed it to Main routes

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -3,11 +3,13 @@ import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 
 import Home from '../pages/Home';
+import NotFound from '../components/NotFound';
 
 export default function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
