78aa65376289061ca79a0504149295c05e463b40
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-09 12:41:45 -0300

    feat: created homepage and added it to be te / route. Added MainRoutes to App

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import {Routes, Route} from 'react-router-dom';
+
+import Home from '../pages/Home';
 
 export default function MainRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 }
