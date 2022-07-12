bc357f3b28784911b8abc1e5b25452e8bf3ecbfa
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-12 12:49:50 -0300

    fix: changed introduction style

---
+++ src/components/Description.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Description() {
+  return (
+    <section>
+      <h2>Description</h2>
+      <p>lorem</p>
+    </section>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import Introduction from '../components/Introduction';
+import Description from '../components/Description';
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Introduction />
+      <Description />
     </main>
   );
 }
