12b41e8b200af07f00ea84ab3463d2ec315d874f
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-12 16:42:21 -0300

    made description projects style

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,10 +1,67 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import AppContext from '../context/AppContext';
+import GenericcProjectCard from './GenericProjectCard';
+
+import {
+  shoppingCartDesc,
+  appIdeasDesc,
+  recipeAppDesc,
+  onlineStoreDesc,
+  trySeriesDesc,
+} from '../data/descriptions';
+import {
+  appIdeas,
+  recipeApp,
+  onlineStore,
+  trySeries,
+  shoppingCart,
+} from '../data/links';
 
 export default function Description() {
+  const {darkMode} = useContext(AppContext);
+
   return (
-    <section>
-      <h2>Description</h2>
-      <p>lorem</p>
+    <section
+      className={`description__container__${darkMode ? 'dark' : 'light'}`}
+    >
+      <section
+        className={`description__container__text__${
+          darkMode ? 'dark' : 'light'
+        }`}
+      >
+        <h2>Projects</h2>
+        <section className="description__container__text__projects">
+          <GenericcProjectCard
+            link={shoppingCart}
+            title="Shopping cart"
+            description={shoppingCartDesc}
+          />
+
+          <GenericcProjectCard
+            link={appIdeas}
+            title="App ideas"
+            description={appIdeasDesc}
+          />
+
+          <GenericcProjectCard
+            link={recipeApp}
+            title="Recipe app"
+            description={recipeAppDesc}
+          />
+
+          <GenericcProjectCard
+            link={onlineStore}
+            title="Online store"
+            description={onlineStoreDesc}
+          />
+
+          <GenericcProjectCard
+            link={trySeries}
+            title="Try series"
+            description={trySeriesDesc}
+          />
+        </section>
+      </section>
     </section>
   );
 }
