ab28d4d5106f0b96565aa79a74aaf93a8ba78e98
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-12 16:38:28 -0300

    added a new data center

---
+++ src/data/descriptions.js
@@ -0,0 +1,9 @@
+export const appIdeasDesc = 'A  app wich contains a collection of apps.';
+
+export const recipeAppDesc = 'A  app wich contains a collection of recipes.';
+
+export const onlineStoreDesc = 'A app that simulate a online store.';
+
+export const shoppingCartDesc = 'A app that simulate a shopping cart.';
+
+export const trySeriesDesc = 'A app that simulate a series site.';
---
+++ src/data/links.js
@@ -0,0 +1,9 @@
+export const appIdeas = 'https://gammarkin.github.io/app-ideas-executions/';
+
+export const recipeApp = 'https://gammarkin.github.io/recipe-app/';
+
+export const onlineStore = 'https://gammarkin.github.io/online-store/';
+
+export const shoppingCart = 'https://gammarkin.github.io/ShoppingCart/';
+
+export const trySeries = 'https://gammarkin.github.io/TrySeries/';
