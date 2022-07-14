c9b1a95f4bbc9c534f04ba782b3e8ba0c597252e
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-13 22:39:00 -0300

    finished the page html

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {Link} from 'react-router-dom';
+import AppContext from '../context/AppContext';
 
 export default function NotFound() {
-  return <div>NotFound</div>;
+  const {darkMode} = useContext(AppContext);
+  return (
+    <main className={`404__container__${darkMode ? 'dark' : 'light'}`}>
+      <h1 className={`404__error__${darkMode ? 'dark' : 'light'}`}>404</h1>
+
+      <p className={`404__error__message__${darkMode ? 'dark' : 'light'}`}>
+        Page not found
+      </p>
+
+      <Link to="/">
+        <p className={`404__error__link__${darkMode ? 'dark' : 'light'}`}>
+          Take me back to home
+        </p>
+      </Link>
+    </main>
+  );
 }
