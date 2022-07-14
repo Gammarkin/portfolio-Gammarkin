83c6ab4b4ed54edf144de26d6255e6abf35c505e
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-13 21:30:57 -0300

    made Project file, and added a new route

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -5,12 +5,14 @@ import {Routes, Route} from 'react-router-dom';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 import AboutMe from '../pages/AboutMe';
+import Projects from '../pages/Projects';
 
 export default function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="about-me" element={<AboutMe />} />
+      <Route path="/about-me" element={<AboutMe />} />
+      <Route path="/projects" element={<Projects />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
