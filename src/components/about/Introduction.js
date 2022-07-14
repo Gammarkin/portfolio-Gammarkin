2f63857969959efd914cade147c31ff6d664d756
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-13 23:05:44 -0300

    added Introduction to about-me

---
+++ src/components/about/Introduction.js
@@ -0,0 +1,45 @@
+import React, {useContext} from 'react';
+import AppContext from '../../context/AppContext';
+import gamma from '../../icons/gamma.jpg';
+
+export default function Introduction() {
+  const {darkMode} = useContext(AppContext);
+  return (
+    <section className={`about__container__${darkMode ? 'dark' : 'light'}`}>
+      <img
+        className={`about__image__${darkMode ? 'dark' : 'light'}`}
+        src={gamma}
+        alt="a young white men with curly hair"
+      />
+
+      <p className={`about__description__${darkMode ? 'dark' : 'light'}`}>
+        Hello, My name is Marcos. I'm a full-stack developer from Brasil. Avid
+        for learning, and a passionate for tech, i'm currently a JavaScript
+        developer with knowledge of React, NodeJS, and Express. I love being
+        part of a team, and I'm always looking for new challenges.
+      </p>
+
+      <p className={`about__email__${darkMode ? 'dark' : 'light'}`}>
+        email: marcosrbnsf@gmail.com
+      </p>
+
+      <a
+        className={`about__link__${darkMode ? 'dark' : 'light'}`}
+        href="https://github.com/Gammarkin"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <p>GitHub</p>
+      </a>
+
+      <a
+        className={`about__link__${darkMode ? 'dark' : 'light'}`}
+        href="https://www.linkedin.com/in/mknmarcosfilho/"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <p>LinkedIn</p>
+      </a>
+    </section>
+  );
+}
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/about/Header';
+import Introduction from '../components/about/Introduction';
 
 export default function AboutMe() {
   return (
     <main>
       <Header />
+      <Introduction />
     </main>
   );
 }
