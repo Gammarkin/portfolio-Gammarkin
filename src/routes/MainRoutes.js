d700512164398c36af89f21a9ff305c2a5f122e2
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-13 22:27:00 -0300

    made route about-me

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -4,11 +4,13 @@ import {Routes, Route} from 'react-router-dom';
 
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
+import AboutMe from '../pages/AboutMe';
 
 export default function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="about-me" element={<AboutMe />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
