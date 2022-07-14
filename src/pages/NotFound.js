75dc33013b3e98f32f5c895d183194fea63b323a
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-13 22:51:43 -0300

    finished notFound error page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,15 +5,15 @@ import AppContext from '../context/AppContext';
 export default function NotFound() {
   const {darkMode} = useContext(AppContext);
   return (
-    <main className={`404__container__${darkMode ? 'dark' : 'light'}`}>
-      <h1 className={`404__error__${darkMode ? 'dark' : 'light'}`}>404</h1>
+    <main className={`error404__container__${darkMode ? 'dark' : 'light'}`}>
+      <h1 className={`error404__${darkMode ? 'dark' : 'light'}`}>404</h1>
 
-      <p className={`404__error__message__${darkMode ? 'dark' : 'light'}`}>
+      <p className={`error404__message__${darkMode ? 'dark' : 'light'}`}>
         Page not found
       </p>
 
       <Link to="/">
-        <p className={`404__error__link__${darkMode ? 'dark' : 'light'}`}>
+        <p className={`error404__link__${darkMode ? 'dark' : 'light'}`}>
           Take me back to home
         </p>
       </Link>
