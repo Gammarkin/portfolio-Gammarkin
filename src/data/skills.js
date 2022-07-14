361f4ae6cc0ba8fb00c9d91090ea6533f1004d9e
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-14 19:05:06 -0300

    made a file to store both skills array

---
+++ src/data/skills.js
@@ -0,0 +1,97 @@
+// My current skills as a developer.
+// <br /> And there's more to come.
+
+const JS_ICON = 'https://pcodinomebzero.neocities.org/Imagens/javascript1.png';
+const REACT_ICON =
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png';
+const DOCKER_ICON = 'https://cdn-icons-png.flaticon.com/512/919/919853.png';
+const SASS_ICON =
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/1280px-Sass_Logo_Color.svg.png';
+const MYSQL_ICON =
+  'https://d1.awsstatic.com/asset-repository/products/amazon-rds/1024px-MySQL.ff87215b43fd7292af172e2a5d9b844217262571.png';
+const REDUX_ICON =
+  'https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png';
+const CSS_ICON = 'https://cdn-icons-png.flaticon.com/512/732/732190.png';
+const HTML_ICON = 'https://cdn-icons-png.flaticon.com/512/732/732212.png';
+const JEST_ICON =
+  'https://symbols.getvecta.com/stencil_85/20_jest-icon.aff64ab210.png';
+
+export const hardSkills = [
+  {
+    name: 'JavaScript',
+    description:
+      'JavaScript is a programming language that runs in the browser.',
+    icon: JS_ICON,
+  },
+
+  {
+    name: 'React',
+    description: 'React is a JavaScript library for building user interfaces.',
+    icon: REACT_ICON,
+  },
+
+  {
+    name: 'Docker',
+    description: 'Docker is a tool for building and managing containers.',
+    icon: DOCKER_ICON,
+  },
+
+  {
+    name: 'SASS',
+    description:
+      'SASS is a CSS preprocessor. With it, you can write styles in a more readable way.',
+    icon: SASS_ICON,
+  },
+
+  {
+    name: 'MySQL',
+    description: 'MySQL is a relational database management system.',
+    icon: MYSQL_ICON,
+  },
+
+  {
+    name: 'Redux',
+    description:
+      'Redux is a JavaScript library for managing application state.',
+    icon: REDUX_ICON,
+  },
+
+  {
+    name: 'CSS',
+    description:
+      'CSS is a stylesheet language used to describe the style of an HTML document.',
+    icon: CSS_ICON,
+  },
+
+  {
+    name: 'HTML',
+    description: 'HTML is the standard markup language for creating Web pages.',
+    icon: HTML_ICON,
+  },
+
+  {
+    name: 'Jest',
+    description: 'Jest is a JavaScript testing framework.',
+    icon: JEST_ICON,
+  },
+];
+
+export const softSkills = [
+  {
+    name: 'Perseverance',
+    description:
+      'Despite the hardships, I will always try my best to reach my goals.',
+  },
+
+  {
+    name: 'Flexibility',
+    description:
+      'I can work in any environment, regardless of the time and place.',
+  },
+
+  {
+    name: 'Teamwork',
+    description:
+      'I am always ready to help my teammates and co-workers. And can fit in any team.',
+  },
+];
