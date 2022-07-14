a001ddb1bb06b52e8cbb84e37473521e335281db
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-13 22:20:00 -0300

    changed Not Found positions, fixed import from route

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 
 import Home from '../pages/Home';
-import NotFound from '../components/NotFound';
+import NotFound from '../pages/NotFound';
 
 export default function MainRoutes() {
   return (
