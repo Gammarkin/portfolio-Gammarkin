5efe5e1532325e223a6f5d51da69950a798b4b01
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-14 19:05:46 -0300

    changed the description

--- src/components/home/Description.js
+++ src/components/home/Description.js
@@ -1,68 +1,58 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import AppContext from '../../context/AppContext';
-import GenericcProjectCard from './GenericProjectCard';
 
-import {
-  shoppingCartDesc,
-  appIdeasDesc,
-  recipeAppDesc,
-  onlineStoreDesc,
-  trySeriesDesc,
-} from '../../data/descriptions';
-import {
-  appIdeas,
-  recipeApp,
-  onlineStore,
-  trySeries,
-  shoppingCart,
-} from '../../data/links';
+import {hardSkills, softSkills} from '../../data/skills';
 
 export default function Description() {
   const {darkMode} = useContext(AppContext);
+
+  const [skillsToUse, setSkillsToUse] = useState(hardSkills);
+  const [hoveringMessage, setHoveringMessage] = useState('');
 
   return (
     <section
       className={`description__container__${darkMode ? 'dark' : 'light'}`}
     >
-      <h2 style={{color: 'white'}}>Projects</h2>
+      <h2 style={{color: 'white'}}>Skills</h2>
       <section
         className={`description__container__text__${
           darkMode ? 'dark' : 'light'
         }`}
       >
-        <section className="description__container__text__projects">
-          <GenericcProjectCard
-            link={shoppingCart}
-            title="Shopping cart"
-            description={shoppingCartDesc}
-          />
-
-          <GenericcProjectCard
-            link={appIdeas}
-            title="App ideas"
-            description={appIdeasDesc}
-          />
-
-          <GenericcProjectCard
-            link={recipeApp}
-            title="Recipe app"
-            description={recipeAppDesc}
-          />
-
-          <div className="doNotShowOnMobile">
-            <GenericcProjectCard
-              link={onlineStore}
-              title="Online store"
-              description={onlineStoreDesc}
-            />
-
-            <GenericcProjectCard
-              link={trySeries}
-              title="Try series"
-              description={trySeriesDesc}
-            />
-          </div>
+        <section>
+          <button type="button" onClick={() => setSkillsToUse(hardSkills)}>
+            Hard-skills
+          </button>
+          <button type="button" onClick={() => setSkillsToUse(softSkills)}>
+            Soft-skills
+          </button>
+          <section>
+            {skillsToUse.map((skill) => {
+              if (skillsToUse === hardSkills) {
+                return (
+                  <img
+                    key={skill.name}
+                    src={skill.icon}
+                    alt={skill.name}
+                    onMouseLeave={() => setHoveringMessage('')}
+                    onMouseEnter={() => setHoveringMessage(skill.description)}
+                  />
+                );
+              }
+              return (
+                <h1
+                  key={skill.name}
+                  onMouseLeave={() => setHoveringMessage('')}
+                  onMouseEnter={() => setHoveringMessage(skill.description)}
+                >
+                  {skill.name}
+                </h1>
+              );
+            })}
+          </section>
         </section>
+
+        <p>{hoveringMessage}</p>
       </section>
     </section>
   );
