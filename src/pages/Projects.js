2e8f06ccd47d384f75913680c0c43184a02f4e8b
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-15 20:00:20 -0300

    made projects preparatives

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/generic/Header';
+import Main from '../components/Projects/Main';
 
 export default function Projects() {
   return (
     <main>
       <Header />
+      <Main />
     </main>
   );
 }
